6434c5ea10ca953f79de7570a1ebd0f7cb73a386
xiaoguangli <guangmvp@gmail.com>
2022-05-24 23:14:51 +1000

    New javascript file

---
+++ html/Javascript/test.js
@@ -0,0 +1,11 @@
+/*var twitter = prompt("Please input your twitter:").slice(0,20);
+var twitterCount = twitter.length 
+
+alert("You have written "+twitterCount+" characters,you have "+(20-twitterCount)+" characters left.");*/
+
+/*var yourName = prompt("Please enter your name:");
+var updateName = yourName.slice(0,1).toUpperCase()+ yourName.slice(1,yourName.length).toLowerCase();
+alert("Hello, "+updateName);*/
+var dogAge = prompt("How old is that dog?");
+var humanAge = (dogAge - 2) * 4 + 21;
+alert("Age of that dog is like a human age that:" + humanAge);
