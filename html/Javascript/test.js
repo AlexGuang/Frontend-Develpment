f415399898fe1180de1fb4e789447adbd6888c34
xiaoguangli <guangmvp@gmail.com>
2022-05-25 23:32:17 +1000

    Stanford Karel CODE

--- html/Javascript/test.js
+++ html/Javascript/test.js
@@ -9,3 +9,42 @@ alert("Hello, "+updateName);*/
 var dogAge = prompt("How old is that dog?");
 var humanAge = (dogAge - 2) * 4 + 21;
 alert("Age of that dog is like a human age that:" + humanAge);
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main() {
+    //your code here
+    moveThree();
+    moveTwo();
+    moveThree();
+    moveTwo();
+    moveThree();
+}
+
+
+function moveThree() {
+    putBeeper();
+    move();
+    move();
+    putBeeper();
+    move();
+    move();
+    putBeeper();
+
+}
+
+function moveTwo() {
+    turnLeft();
+    move();
+    turnLeft();
+    move();
+    putBeeper();
+    move();
+    move();
+    putBeeper();
+    move();
+    turnRight();
+    move();
+    turnRight();
