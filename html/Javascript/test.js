1f3053e0a6b52a47cdfd70a98b6e28fa7ffe23bf
xiaoguangli <guangmvp@gmail.com>
2022-05-26 22:44:25 +1000

    Completed the test javascript file

--- html/Javascript/test.js
+++ html/Javascript/test.js
@@ -70,3 +70,78 @@ function getMilk() {
     console.log("moveLeft");
     console.log("enterHouse");
 }
+
+function bmiCalculator(weight, height) {
+    var bmi = weight / Math.pow(height, 2);
+    var interpretation;
+    if (bmi < 18.5)
+
+    { interpretation = "Your BMI is <" + bmi + ">, so you are underweight."; } else if (bmi >= 18.5 && bmi <= 24.9) {
+        interpretation = "Your BMI is <" + bmi + ">, so you have a normal weight.";
+    } else if (bmi > 24.9) {
+        interpretation = "Your BMI is <" + bmi + ">, so you overweight.";
+    }
+    return interpretation;
+}
+console.log(bmiCalculator(100, 1.73));
+
+
+//check a guest if in the list
+var guestList = ["Frank", "Marry", "Jack", "Coco", "Rose", "Lora"];
+var newComing = prompt("Please enter your name:");
+if (guestList.includes(newComing)) {
+    alert("Welcome!");
+} else {
+    alert("Sorry, you are not invited.");
+}
+
+var arryGame = [];
+var number = 0;
+
+function fizzBuzz() {
+    number++;
+    if (number % 3 === 0) {
+        if (number % 15 === 0) {
+            arryGame.push("FizzBuzz");
+        } else {
+            arryGame.push("Fizz")
+        }
+    } else if (number % 5 === 0) {
+        if (number % 15 === 0) {
+            arryGame.push("FizzBuzz");
+        } else
+            arryGame.push("Buzz");
+    } else {
+        arryGame.push(number);
+    }
+
+    console.log(arryGame);
+}
+
+
+
+var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+
+function whosPaying(names) {
+
+    /******Don't change the code above*******/
+
+    //Write your code here.
+
+    var friends = names;
+    var index = Math.floor(Math.random() * 5);
+    console.log("index is: " + index);
+    return friends[index] + " is going to buy lunch today!";
+
+
+
+
+    /******Don't change the code below*******/
+}
+
+console.log(whosPaying(names));
+
+
+//Lyric of the song:99 bottle of beer
+console.log(numberBottles + " bottles of beer on the wall, " + numberBottles + " bottles of beer.");
+console.log("Take one down and pass it around, " + numberBottles - 1 + " bottles of beer on the wall");
