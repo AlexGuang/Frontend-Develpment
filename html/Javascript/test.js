2510f17bd649c8658c239eaf84132404419fc325
xiaoguangli <guangmvp@gmail.com>
2022-05-26 14:01:40 +1000

    Updated file js

--- html/Javascript/test.js
+++ html/Javascript/test.js
@@ -48,3 +48,25 @@ function moveTwo() {
     turnRight();
     move();
     turnRight();
+}
+
+function getMilk() {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
