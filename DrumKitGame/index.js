fe51fd7a1984e95a76fa134b8a8137a4dbb159c9
xiaoguangli <guangmvp@gmail.com>
2022-05-29 23:11:43 +1000

    Drum sound and image

--- DrumKitGame/index.js
+++ DrumKitGame/index.js
@@ -1,5 +1,10 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function() {
-        alert("I got clicked!");
+        this.textContent.style.color = "white";
     });
 }
+
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+//         audio.play();
