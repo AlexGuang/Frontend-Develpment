9858900f52b7bc314ae0b966261ecbcc30425d56
xiaoguangli <guangmvp@gmail.com>
2022-06-02 14:38:54 +1000

    Add event listener with jQuery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,10 @@
-$("h1").css("color", "red");
+$("h1").css("color", "blue");
+$(document).keydown(function(event) {
+    $("h1").text(event.key);
+});
+$("h2").on("mouseover", function() {
+    $("h2").css("color", "red");
+});
+$("h2").on("mouseout", function() {
+    $("h2").css("color", "yellow");
+});
