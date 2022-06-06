663293641b4587b29046a2b22c4dd2a23d40f727
xiaoguangli <guangmvp@gmail.com>
2022-06-06 12:49:27 +1000

    Creat other two routers of hobbies and about

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -5,8 +5,19 @@ const app = express();
 
 app.get("/", function(req, res) {
 
-    response.send("<h1>Hello World!</h1>")
+    res.send("<h1>Hello World!</h1>");
+});
+app.get("/contact", function(req, res) {
+    res.send("guangmvp@gmail.com");
+
+});
+app.get("/hobbies", function(req, res) {
+    res.send("<h1>I love gaming, movies and coding</h1>")
 })
+
+app.get("/about", function(req, res) {
+    res.send("<h2>I am Alexander, I am a professional software developer</h2>");
+});
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
