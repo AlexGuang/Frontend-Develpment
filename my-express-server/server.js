90495558ae2f127fbec012bbe4f8f40b8674e2b5
xiaoguangli <guangmvp@gmail.com>
2022-06-06 12:15:56 +1000

    Use callback function and get method to response request from brower.

---
+++ my-express-server/server.js
@@ -0,0 +1,12 @@
+//jshint esversion:6
+const { response } = require("express");
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+
+    response.send("<h1>Hello World!</h1>")
+})
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
