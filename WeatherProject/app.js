60a0e0af3f1fadb56a80bf67c7d3d68688ce65d4
xiaoguangli <guangmvp@gmail.com>
2022-06-08 23:15:25 +1000

    Change some parameters

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -6,10 +6,10 @@ const app = express();
 
 app.get("/", function(req, res) {
     //res.send("Server is up and running");
-    var urlApi = "https://api.openweathermap.org/data/2.5/weather?q=Hobart&appid=172747710e80a10c8894d321db5f8180&units=metric";
+    var urlApi = "https://api.openweathermap.org/data/2.5/weather?q=Beijing&appid=172747710e80a10c8894d321db5f8180&units=metric";
 
     https.get(urlApi, function(response) {
-        console.log(response.statusCode);
+        //console.log(response.statusCode);
 
         response.on("data", function(data) {
             //console.log(data);
