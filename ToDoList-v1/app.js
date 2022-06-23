bec1d1b73629cff45850c4de262800c95c141c36
xiaoguangli <guangmvp@gmail.com>
2022-06-23 13:16:14 +1000

    Fetch data from mogodb and show in the ejs page

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const data = require(__dirname + "/data.js");
+const mongoose = require("mongoose");
 
 
 
@@ -9,16 +10,55 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+mongoose.connect("mongodb://127.0.0.1:27017/toDoList");
+
+const itemSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:true
+    }
+}
+);
+
+const Item = mongoose.model("Item",itemSchema);
 
 
-const items = ["学前端", "到前屋接水", "做饭", "吃饭"];
+
+const study = new Item({
+    name:"Study"
+});
+const fetchWater = new Item({
+    name:"Go to fetch water."
+});
+const cook = new Item({
+    name:"Cooking"
+   
+});
+const eating = new Item({
+    name:"Eating"
+});
+
+// Item.insertMany([study,fetchWater,cook,eating], function(err){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("Succefully launched!");
+//     }
+// });
+Item.find({},function(err,result){
+    console.log(result);
+});
+
+
 const workItems = ["做网页"];
 
 app.get("/", function(req, res) {
     const day = data.getDate();
-    res.render("list", { pageTitle: day, toDoItems: items });
-
-});
+    Item.find({},function(err,result){
+        res.render("list", { pageTitle: day, toDoItems: result });
+    });
+   });
 app.post("/", function(req, res) {
     console.log(req.body);
     if (req.body.submitItem === "Work") {
