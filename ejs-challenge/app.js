89783fc59ac18f52c9024d7bfd5896f3369a2050
xiaoguangli <guangmvp@gmail.com>
2022-06-30 12:46:58 +1000

    Complete moving data from local storage to database

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -7,10 +7,6 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/myBlog")
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -46,37 +42,86 @@ const contactBlog = new Blog({
 
 });
 
-
-
-const posts =[];
-let paraContents;
-Blog.insertMany([homeBlog,boutBlog,contactBlog],function(err,docs){
-  if(err){
-    console.log(err);
-  }
-  else{
-    console.log("Successfully inserted!");
+const postSchema = mongoose.Schema({
+  title:{
+    type:String,
+    required:true
+  },
+  content:{
+    type:String,
+    required:true
   }
 });
 
+const Post = mongoose.model("Post",postSchema);
+
+
 app.get("/",function(req,res){
-  res.render("home",{homeStartingContentPara:homeStartingContent,postsArray:posts});
+  Blog.findOne({title:"Home"},function(err,result){
+    if(err){
+      console.log(err);
+    }
+    else if(!result){
+      Blog.insertMany([homeBlog,boutBlog,contactBlog],function(err,docs){
+        if(err){
+          console.log(err);
+        }
+        else{
+          console.log("Successfully inserted!");
+        }
+        res.redirect("/");
+      });
+    }
+    else{
+      Post.find({},function(err,resul){
+        if(err){
+          console.log(err);
+        }
+        else
+        {
+          res.render("home",{homeStartingContentPara:result.content,postsArray:resul});
+        }
+      });     
+      
+    }
+  });
+  
 });
 
 
 app.get("/about",function(req,res){
-  res.render("about",{aboutPageContent:aboutContent});
+  Blog.findOne({title:"About"},function(err,docs){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("about",{aboutPageContent:docs.content});
+    }
+  });
+  
 });
 app.get("/contact",function(req,res){
-  res.render("contact",{contactPageContent:contactContent});
+  Blog.findOne({title:"Contact"},function(err,docs){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("contact",{contactPageContent:docs.content});
+    }
+  });
 });
 app.get("/compose",function(req,res){
   res.render("compose");
 });
 app.post("/compose",function(req,res){
-  const newPost ={title:req.body.inputTitle,
-  postBody :req.body.textarea};
-  posts.push(newPost);
+
+const newPostLog = new Post({
+  title:req.body.inputTitle,
+  content:req.body.textarea
+});
+newPostLog.save();
+
+  // const newPost ={title:req.body.inputTitle,
+  // postBody :req.body.textarea};
+  // posts.push(newPost);
   res.redirect("/");
 });
 app.get("/posts/:topics",function(req,res){
